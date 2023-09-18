61c89ed384e556651c3201a2b9f583291072d346
Abhishek Goyal <51013750+abhishekGoyal274@users.noreply.github.com>
2023-09-18 13:14:44 +0530

    Add files via upload

---
+++ src/config/data.js
@@ -0,0 +1,87 @@
+export const blogList = [
+  {
+    id: 1,
+    title: 'Death Note Analysis',
+    category: 'Action',
+    subCategory: ['Mystery', 'thriller', 'Supernatural'],
+    description:
+      "A Quick Rundown Of What Death Note is About: So Death Note is an anime and a manga series which my friends I am working towards purchasing. It follows our main character Light Yagami who is a high school student and one day he discovers a note book from a Shinigami name Ryuk. However, this notebook isnt like any ordinary one it has the power who;s name and face that Light knows leading him to pursue his aim to cleanse the world of either. But that’s not easy when he has Japan’s greatest detective L standing in his way. Now typically speaking Death Note is pretty much your usual cat and the mouse chase between detective L and Criminal with the Death Note Light. However, it has such a dark twist to it and has a rather debatable topic about morals and justice. It’s all sparked off from Light Yagami’s vision of where he hopes to create a utopia cleansed from evil where he is the god leading criminals to be punished raising the question is this morally right to do? It presents a large debate that isn’t so easily answered and this was key aspect I particularly loved about Death Note the fact it has the ability to make me question things.\n\nHowever, although we have this topic brought forward there were so many other strong points including the way the story unravels and that cat the mouse theme was played out so well that I was on the edge of my seat. It’ so twisted and unpredictable in many ways because you genuinely do not know what is going to happen next! Now the second arc was where I felt like the anime was slightly dragging. After the draw dropping ending to the first one and with myself still trying to process what happened I ended up feeling rather deflated of how they had managed to introduce N into the story being an exact replica of L. But this arc may have dragged on but it still contributed a lot to the story in many ways and helped draw the cat and the mouse chase to an end.   ",
+    authorName: 'Amritpal',
+    authorAvatar: '/assets/images/a1.jpg',
+    createdAt: 'June 03, 2022',
+    cover: '/assets/images/death_note.jpg',
+  },
+  {
+    id: 2,
+    title: 'The Brilliance of Naoki Urasawa "Monster"',
+    category: 'Monster,thriller',
+    subCategory: ['vacation', 'holidays', 'sight seeing'],
+    description:
+     "Monster is a sombre, twisted look into the life of one man and the events that unfold under his influence. It takes us down into the seedy underbelly of a post-Berlin Wall 1990s Germany, where the country is shaken by the rise of a serial killer.\n\nOur tale starts with Doctor Tenma, a genius neurosurgeon in Düsseldorf. He has it all: the rich fiancée, Eva, the prestige, and the skill. One night, a little boy comes into the emergency room, gunshot to the head, and only Tenma’s ability can save him. Nine years later, a string of murders occurs and sweeps Tenma up into the brutality. The killer is that very same boy from all those years ago, and the murders are the mere tip of his machinations. What would you do if you had saved the life of the next Hitler?.Tenma feels a personal responsibility, especially with how much attention Johan favours him with, a truly messed up connection akin to Dumbledore and Voldemort. To worsen matters, Detective Lunge is on the case and his primary suspect is Tenma, for he has gained much since the murders. Lunge is a bloodhound, hell-bent on solving the case, even at the cost of all personal feelings and livelihood – a darker Sherlock Holmes.\n\nHe’s the creepy guy you want in charge of your case, but wouldn’t invite over for dinner. I like his strange quirk where he types with his fingers in the air to archive every detail to memory.The opening sequence sets the tone for Monster without a single word or lyric. Tenma keeps looking over his shoulder at distant, ethereal voices, yet there is never anything around once he looks. He is stalked and alone on his quest for the elusive killer.The true star of Monster is the villain, Johan, a Hannibal Lecter type. He reveals himself to us early, but for much of the narrative, he remains in the shadows, controlling the events with unparalleled skill. Importantly, his ability to manipulate countless others and their organisations is believable, most prominently when we see him play his game in person.\n\nI cannot emphasise how much you must watch Monster. That said, I would wait until you have developed a liking for lengthy dramas, or you may miss much of the subtlety that makes Monster unforgettable, as I had in my youth. Remember to pay attention at all times. ",
+    authorName: 'Amritpal',
+    authorAvatar: '/assets/images/a1.jpg',
+    createdAt: 'July 06, 2021',
+    cover: '/assets/images/monster.jpg',
+  },
+  {
+    id: 3,
+    title: 'Hunter-X-Hunter',
+    category: 'Supernatural',
+    subCategory: ['e-commerce store', 'clothing', 'shopping store'],
+    description:
+      "When I was younger I used to be really into anime. The amount of anime I have watched totals to about 50, and I can honestly say Hunter x Hunter is one of around five anime I do not regret watching at least a little, the others being JJBA, One Punch Man, NGE, and Soul eater. I carry a lot of regret for other titles I've watched, but hxh holds as one of the best shows I've ever watched.\n\nCharacters: The characters alone merit this show deserving a 10. Every single character is deep, dynamic, and interesting. The main characters, Gon, Killua, Kurapika, and Leorio are all unique and contribute greatly to both the narrative and intrigue of the show. Every villain receives development most shows rob villains of. Every character feels like a real person, with real emotions and reasons for the decisions they make. I can hardly think of another show that does characters as well as hxh.\n\nWorld: Frankly, I can only think of two pieces of media that have EVER done world building as well as hxh. Those are Homestuck and Avatar: The Last Airbender. hxh can't really be trumped when it comes to world building. The magical beasts, crime-ridden cities, deep network of influential and ominous criminals, unexplored lands, heroic hunters, and fiercely powerful beings all blend seamlessly into a world that I can hardly help but think extensively about every once in a while when I'm bored or my mind wanders.\n\nPlot: Like most longer anime, hxh follows story arcs that tend to be rather distinct from each other. The direction in the beginning very clearly follows Gon's desire to find his dad. As any good anime should do, hxh has reason for, and connection between, each arc. Each of the main characters has their own distinct goals, and it would spoil part of the show to elaborate beyond that, but the goals of each character culminate brilliantly to advance the plot of the series.\n\nOverall, hxh is deserving of a 10 in every single area. In my own opinion, it's the best anime I have seen to date, as well as one of the best shows I have ever seen. Watch hxh to be exposed to a beautiful and rich new world, with deep characters that leave a lasting impact on the viewer. Watch hxh; I promise you won't regret it.",
+    authorName: 'Amritpal',
+    authorAvatar: '/assets/images/a1.jpg',
+    createdAt: 'July 13, 2021',
+    cover: '/assets/images/hunter.jpg',
+  },
+  {
+    id: 4,
+    title: 'My Hero Academia',
+    category: 'adventure',
+    subCategory: ['adrenaline', 'stay-fit', 'lifestyle'],
+    description:
+      "Story - 8/10 The thing is, HeroAca has almost two stories going on at the same time. The overarching story of random weak nerd gets special power and becomes a hero which, as far as things go, is pretty cliché. Not bad, but really cliché. And then we have the more intricate story - the story of the people Deku meets and how they affect him or vice verse, and then we see just how vulnerable and imperfect this society is, and not even superpowers can stop all the problems. The show is very good at not only building its own, new version of our world, but then going ahead and facing the problems and real-world issues it suffers.\n\nCharacters - 10/10 HeroAca has a pretty big cast of characters, one that only grows in season 2. Despite this, the creator of the HeroAca manga that HeroAca is based of, Kori Horikoshi, has done an excellent job of never sidelining any characters and making them helpful whenever the story allows it. Now, Season 2 doesn't have time to give everyone the spotlight, but it certainly does to a good chunk of the cast. And while not everyone is getting developed as such, they are becoming more and more lashed out as characters and obtaining their own stand out personalities. And the characters that do get developed... oh god, they develop them right. Obviously not all characters have gotten fleshed out yet, but as a manga reader I know they all will so I'm giving out the 10 rating.\n\nSound - 9/10 There are a lot of OSTs that are absolutely amazing and serve as brilliant accompaniment to whatever scene they're in. Most of the soundtrack can be listened to on their own, without the context of the show, and still be epic. However some of the songs don't have much variety, which does knock it down a few points. Then we have the voice actors - something I forgot to mention in my Made In Abyss review - and oh boy the voice actors are amazing. While I personally prefer sub, as I think Deku's VA expresses much more emotion, the dub is extremely solid and one of the best in a while. It's also very good at not changing really important lines which is always appreciated.",
+    authorName: 'Amritpal',
+    authorAvatar: '/assets/images/a1.jpg',
+    createdAt: 'July 29, 2021',
+    cover: '/assets/images/mha.jpg',
+  },
+  {
+    id: 5,
+    title: 'Demon Slayer',
+    category: 'Dark fantasy',
+    subCategory: ['bbq', 'food', 'lifestyle'],
+    description:
+      "I have seen a lot of action fantasy anime shows, but for some reason, this is the best. I was so into it that I can't pick one just thing as my favorite about this show. The action, the character development, the emotional moments, the score, the animation or be it just the writing in general, Demon Slayer is on a whole different level. I went through a number of emotions watching it, and it felt like I was on one wild ride from episode 1 to 26. The mood keeps varying from time to time, but the tempo never drops and with intriguing characters, it only gets better and better with every episode.Demon Slayer keeps shifting focus to different supporting characters, thus making you feel as if you're yourself a part of the protagonist Tanjiro's journey: one filled with tons of mind-blowing, jaw-dropping action sequences. There were a number of moments where my mind was blown away! Amidst all the violence and tension, the sweet brother-sister relationship of Tanjiro and Nezuko tugs at your heartstrings. While Tanjiro is the usual memorable and relatable shonen protagonist, other characters are surprisingly also treated very well. Be it the cute Nezuko, the tough yet hilarious Inosuke, or the comic relief yet strongly written Zenitsu, the show is full of characters that will end up growing on you.The animation is literally breathtaking. Several action scenes feel like actually filmed with a camera than drawn by hand, which is stupendous! The score is another major positive, elevating the show's epic scale and tone to unexpected levels. Although it's not a comedy, the funny bits involving the three boys are a major highlight, especially in the last few episodes. Some might find its quickly shifting focus from one set of supporting characters to another a little uneven, but the writing is so strong you don't get time to think anything else, with constant action delivered in every episode.I am normally left blank when I finish watching an anime show, but that's not what happened here. I was instead happy that it was perfectly concluded and it left me excited for the box-office blockbuster sequel film, which I clearly can't wait to watch now. Demon Slayer is the best anime I've seen to date, and something I'd love to watch again and again.",
+    authorName: 'Amritpal',
+    authorAvatar: '/assets/images/a1.jpg',
+    createdAt: 'July 30, 2022',
+    cover: '/assets/images/demon.jpg',
+  },
+  {
+    id: 6,
+    title: 'Attack on Titan',
+    category: 'Thriller',
+    subCategory: ['beaches', 'sea', 'holidays'],
+    description:
+      "Story 10/10\n\n It continunes few days after where we left off last season and it focuses more on political drama and dilemmas inside the walls. This season gets straight to the point and introduces new villain named Kenny that turns out to be somehow related to Levi. There are many parties trying to get Eren and Historia. For what reason? It is yet to be reveald. Mystery we all feel in love with is still there and there are even more questions, but don't let that distract you from the fact that this season ,even tho is only few episodes in, already has some major reveals in it. Pacing is amazing and i wasn't bored for a moment while watching. Things are only getting more interesting when Erwin reveals how he's planning to ovethrow the governmant without using the force. This season has a very refreshing and different plot compared to previous ones as we can see from the lack of titans and focuse on the issues inside the walls rather than beyond. However it's still as, if not more interesting than before. There is world buidling,mystery,back stabbing and some major questions answered which there is hopfully more to come.\n\nAnimation 10/10\n\nAs we can see characters in this season look a little bit different or older which some might not like but i do personally. It's a fresh change. They look closer to their manga designes. Some might notice that they removed those thick black lines around characters faces which made AOT art stand out even more. Don't get me wrong, it still does but that was something that was missing which kept me from giving it 10/10. Animation is brilliant for this season (it's Attack on Titan after all :D) and like every season it only got better. . Perfect combination between 3D background and 2D animation along with masterfull camera movement made this scene look like a movie. There is even one frame where levi is facing the wall and you can see his reflection in the window. Just goes to show how much detail and time was put in to this scene. After i done some research i found out that it took the animator of this scene 1 whole month to complete it. Just that one scene alone. All props to Arifumi Imai. Great job. Hopfully it keeps this quality because this season is said to be 24 episodes long.\n\nWhat i like about AOT characters is the fact that they interact so naturally and nothing from their conversation seems forced which is immwdiately showcased in the very first episode.",
+    authorName: 'Amritpal',
+    authorAvatar: '/assets/images/a1.jpg',
+    createdAt: 'July 31, 2022',
+    cover:
+      '/assets/images/titan.jpg',
+  },
+  {
+    id: 7,
+    title: 'Steins:Gate',
+    category: 'Sc-fi',
+    subCategory: ['skill', 'design', 'passion'],
+    description:
+      "Tutturu~! It took me awhile to finish this review, school, other online drama and whatnot. So here is my in depth evaluation/praise of the show Steins;Gate. I must say that I have excluded a lot of spoiling content so that I can have a legitimate review posted. This is my first review and I hope it informs you and gives you a different perspective of the show!.The genre of Steins;Gate, from what I observed, was Sci-Fi, Thriller, and Drama.\n\nThe demography reaches out to both Shonen and Seinen crowds. The concepts of time traveling, time paradox, time looping, time leaping, and the butterfly effect were all presented very well. They were explained with a fluent, consistent logic and the concepts are supported by using time lines (in the show they call it world lines, when really it’s almost saying “parallel universe”.) Some may criticize Okabe’s ability to retain his memories through his “Reading Steiner”, which I personally choose to ignore for reasons I will say at the end of the review.The setting is in modern day society and the conflict is between our main characters and a secret organization called SERN. The whole SERN conflict shows this kind of Terminator-feel to the overall plot and Okabe is basically John Connor in retrospect to the beloved Terminator series. Overall, the story is greatly supported by the several types of conflicts and plot progression being utilized all throughout the series. Steins;Gate also consists of addicting suspense that supplements the story with a fluent, chain-linked consistency that makes you crawl back for more—a well done job. Characters are usually all too static in anime, rather than dynamic such as those you find in some Hollywood produced dramas. Here in this particular series, there is a gold mine of dynamic-range for personality and human-quality. A lot of the characters share this quality and all have some elements that any one person could relate to somehow.",
+    authorName: 'Amritpal',
+    authorAvatar: '/assets/images/a1.jpg',
+    createdAt: 'August 5, 2022',
+    cover: '/assets/images/steins.jpg',
+  },
+];
